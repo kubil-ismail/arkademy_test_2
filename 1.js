fedfc2be80e90bf6ecc04015b02fb2ac9e886518
Bilkis Ismail <bilkisismail07@gmail.com>
2020-04-18 10:25:28 +0700

    stable code

---
+++ 1.js
@@ -0,0 +1,50 @@
+// Profile Function get parameter
+const profile = (name = 'Bilkis Ismail', age = 18) => {
+    let jsonData = {
+        name: name,
+        age: age,
+        address: "Kp Pondok Arum, Karawaci, Kota Tangerang, Banten",
+        hobbies: ['Music', 'Football'],
+        is_married: false,
+        list_school: {
+            high_school: {
+                name: "SMKN 4 Kota Tangerang",
+                year_in: "2017",
+                year_out: "2020",
+                major: null,
+            },
+            middle_school: {
+                name: "MTS Daarul Irfan",
+                year_in: "2014",
+                year_out: "2017",
+                major: null,
+            },
+            primary_school: {
+                name: "MI Al-Husna",
+                year_in: "2009",
+                year_out: "2014",
+                major: null,
+            }
+        },
+        skills: {
+            php: "advanced",
+            laravel_lumen: "advanced",
+            codeigniter: "advanced",
+            bootstrap: "advanced",
+            coreldraw: "advance",
+            adobe_xd: "advance",
+            wordpress: "advanced",
+            javascript: "advanced",
+            jquery: "advanced",
+            mysql: "beginner",
+            react: "beginner",
+            linux: "beginner"
+        },
+        interest_in_coding: true,
+    };
+
+    return JSON.stringify(jsonData);
+};
+
+const runProfile = profile('Bilkis Ismail', '18');
+console.log(runProfile);
---
+++ 2.js
@@ -0,0 +1,19 @@
+const usernameCheck = (input) => {
+    // At least 2 to 12 characters, the first letter must be @
+    let regex = /^[@]+[a-zA-Z\d@]{1,11}$/;
+    return regex.test(input);
+};
+
+const passwordCheck = (input) => {
+    // At least 6 characters, only number
+    var regex = /^[0-9]{6,6}$/;
+    return regex.test(input);
+};
+
+const runUsername = usernameCheck("@koders");
+// const runUsername = usernameCheck("@programmerhandal");
+
+const runPassword = passwordCheck("212223");
+// const runPassword = passwordCheck("2!2a3B");
+
+console.log(`Username Check = ${usernameRun} and Password Check = ${passwordRun}`);
